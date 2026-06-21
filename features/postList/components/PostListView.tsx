4689b40c9badbc53637820f69a11e72087b190f5
y7oon <seung.yoon.yu@gmail.com>
2026-06-21 20:09:16 +0900

    feat: 내비게이션 및 리스트 디자인 변경

--- features/postList/components/PostListView.tsx
+++ features/postList/components/PostListView.tsx
@@ -1,33 +1,46 @@
-import type { NotionPageMeta } from 'features/notion/types'
-import { getNotionPageMeta } from 'features/notion/utils/meta/getNotionPageMeta'
-import Link from 'next/link'
-import * as css from './PostListView.css'
+import type { NotionPageMeta } from "features/notion/types";
+import { getNotionPageMeta } from "features/notion/utils/meta/getNotionPageMeta";
+import Link from "next/link";
+import * as css from "./PostListView.css";
 
 interface PostListViewProps {
-  posts: NotionPageMeta[]
+  posts: NotionPageMeta[];
 }
 
 export function PostListView({ posts }: PostListViewProps) {
   return (
     <div className={css.postListFrame}>
       <div className={css.viewLink}>
-        {posts.map(p => (
+        <PostListView.TitleRow />
+        {posts.map((p) => (
           <PostListView.Row key={p.id} meta={p} />
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 PostListView.Row = ({ meta }: { meta: NotionPageMeta }) => {
-  const postMeta = getNotionPageMeta(meta)
-  const year = postMeta.date.slice(0, 4)
-  const path = `/${year}/${postMeta.slug}`
+  const postMeta = getNotionPageMeta(meta);
+  const year = postMeta.date.slice(0, 4);
+  const path = `/${year}/${postMeta.slug}`;
   return (
     <li className={css.postLinkFrame}>
       <Link href={path} className={css.postLinkInner}>
+        <span className={css.postLinkDate}>
+          {postMeta.date.split("-").join(".")}
+        </span>
         <span className={css.postLinkTitle}>{`${postMeta.title}`}</span>
       </Link>
     </li>
-  )
-}
+  );
+};
+
+PostListView.TitleRow = () => {
+  return (
+    <div className={css.postTitleRow}>
+      <span className={css.postLinkDate}>DATE</span>
+      <span className={css.postLinkDate}>TITLE</span>
+    </div>
+  );
+};
